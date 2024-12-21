fd4f4c0b4286c070f366e7c174a7dd785c5bc938
Kalpak P S <kalpak9446@gmail.com>
2024-12-21 12:14:58 +0530

    loading animation

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,13 +1,18 @@
 import './Loading.css'
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const Loading = ({isLoading}) => {
     
     if (isLoading) {
         return (
+            
             <div className='loading-page'>
-                <div className='spinner'>
-                    <h1>Loading Page</h1>
-                </div>
+                <DotLottieReact
+                src="https://lottie.host/0e4adda4-abce-44d0-8389-d0d9323436f2/7ryAgclWH4.lottie"
+            loop
+            autoplay
+            />
             </div>
         )
     }
