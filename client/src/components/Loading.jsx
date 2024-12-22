daa1f28520a890a83c833af43c4f552715c80017
Antony <13antonysun@gmail.com>
2024-12-22 10:59:16 +0530

    loading icon css * 2

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -3,18 +3,11 @@ import React from 'react';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const Loading = ({isLoading}) => {
-
-    const iconStyle = {
-        width: 150,
-        height: 100,
-        alignItems: 'center'
-    }
     
     if (isLoading) {
         return (
             <div className='loading-page'>
-                <DotLottieReact src="https://lottie.host/0e4adda4-abce-44d0-8389-d0d9323436f2/7ryAgclWH4.lottie" loop autoplay
-                style={iconStyle}
+                <DotLottieReact src="https://lottie.host/0e4adda4-abce-44d0-8389-d0d9323436f2/7ryAgclWH4.lottie" loop autoplay className='lottie-icon'
                 />
             </div>
         )
