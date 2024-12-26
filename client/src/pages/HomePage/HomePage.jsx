0752f59ead88e5dee436c55470bd21e2d8e27077
Kalpak P S <kalpak9446@gmail.com>
2024-12-26 13:42:46 +0530

    Add Contact and FAQ components to HomePage

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
 import './HomePage.css';
+import Contact from '../../components/Faq/Contact'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import About from '../../components/About/About';
 
@@ -10,6 +11,7 @@ const HomePage = () => {
         <div className="homepage" style={{ backgroundColor: '#11142E', color: 'white' }}>   
             <Hero />
             <About />
+            <Contact />
         </div>
     );
 }
