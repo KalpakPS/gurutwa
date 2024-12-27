cdc4e51c81e58259ff997fc6ab509a0a7a95fd76
Antony <13antonysun@gmail.com>
2024-12-27 13:14:05 +0530

    navbar trans

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,57 +1,62 @@
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
+import { useState, useEffect } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
 const NavBar = () => {
-    // const [scrolled, setScrolled] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+    
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+            console.log('Scroll Y:', currentScrollY);
+            if (currentScrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
 
-    // useEffect(() => {
-    //     const handleScroll = () => {
-    //         const offset = window.scrollY;
-    //         if (offset > 50) {
-    //             setScrolled(true);
-    //         } else {
-    //             setScrolled(false);
-    //         }
-    //     };
+        window.addEventListener('scroll', handleScroll);
 
-    //     window.addEventListener('scroll', handleScroll);
-    //     return () => {
-    //         window.removeEventListener('scroll', handleScroll);
-    //     };
-    // }, []);
-
-    // let navbarClasses = ['navbar', 'sticky-top'];
-    // if (scrolled) {
-    //     navbarClasses.push('scrolled');
-    // }
-
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
-        <>
-        <Navbar bg="dark" variant="dark" expand="lg" className="navbar-custom">
-        {/* className={navbarClasses.join(" ")} */}
+        <Navbar
+            expand="lg"
+            className={`navbar-custom ${isScrolled ? 'scrolled' : ''} sticky-top`}
+        >
             <Navbar.Brand href="/" className="ps-3">
-            <img src="/GURUTWA logo white.png" 
-            alt="Logo" 
-            width="150" 
-            height="48" 
-            class="d-inline-block align-text-top" 
-            />
+                <img
+                    src="/GURUTWA logo white.png"
+                    alt="Logo"
+                    width="150"
+                    height="48"
+                    className="d-inline-block align-text-top"
+                />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/about">About</Nav.Link>
-                    <Nav.Link as={Link} to="/events">Events</Nav.Link>
+                    <Nav.Link as={Link} to="/">
+                        Home
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/about">
+                        About
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/events">
+                        Events
+                    </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
+    );
+};
 
-        </>
-    )
-}
+export default NavBar;
 
-export default NavBar
