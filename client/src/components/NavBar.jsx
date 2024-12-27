363cd229e6ed8ca65a6bdc3846a6134e902d44cc
Kalpak P S <kalpak9446@gmail.com>
2024-12-27 10:25:14 +0530

    Add scroll effect to NavBar: change background color on scroll

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,19 +1,43 @@
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+import './NavBar.css'; // Import the CSS file
 
 const NavBar = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY;
+            if (offset > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    let navbarClasses = ['navbar', 'sticky-top'];
+    if (scrolled) {
+        navbarClasses.push('scrolled');
+    }
 
     return (
         <>
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="lg" className={navbarClasses.join(" ")}>
             <Navbar.Brand href="/" className="ps-3">
             <img src="/GURUTWA logo white.png" 
             alt="Logo" 
             width="150" 
             height="61" 
-            class="d-inline-block align-text-top" 
+            className="d-inline-block align-text-top" 
             />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -25,9 +49,8 @@ const NavBar = () => {
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
-
         </>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
