d8215f6a8fa69e5bb5528ca56b6a4cc3a3e76651
Kalpak P S <kalpak9446@gmail.com>
2024-12-29 16:35:43 +0530

    Navbar.css import correction

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import './NavBar.css';
 
 const NavBar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
